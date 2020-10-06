672102770ec8e8925ea3080d88f1089d322efde9
Максим Толстобров <maximah1989@yandex.ru>
2020-10-06 15:29:25 +0300

    Create some tasks

---
+++ src/Tasks4/Task5.java
@@ -0,0 +1,13 @@
+package Tasks4;
+
+public class Task5 {
+    public static void main(String[] args) {
+
+        /*
+
+        В данной программе нужно создать цикл while.
+
+         */
+
+    }
+}
